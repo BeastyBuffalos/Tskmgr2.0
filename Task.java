1ad5218c7c48383efc034b487967d839627d787b
Jonah Schulte <jhschult@c28-0117-09.ad.mtu.edu>
2018-02-21 13:23:26 -0500

    Adding the Class Task, with all of the getters and setters + constructor, with will be used for each task created by the program

---
+++ Task.java
@@ -0,0 +1,102 @@
+
+public class Task {
+
+	int duedate = 0;
+
+	String name = " ";
+
+	String type = " "; 
+
+	int hourstodo = 0;
+
+	boolean complete = false;
+
+	public Task(String name1, String type1, int due, int hours, boolean comp) {
+		
+		name = name1;
+		type = type1;
+		duedate = due;
+		hourstodo = hours;
+		complete = comp;
+
+	}
+
+	public void setDue(int due) {
+
+		duedate = due;
+
+	}
+
+	public int getDue() {
+
+		return duedate;
+
+	}
+
+	public void setName(String name1) {
+
+		name = name1;
+
+	}
+
+	public String getName() {
+
+		return name;
+
+	}
+
+	public void setHours(int hours) {
+
+		hourstodo = hours;
+
+	}
+
+	public int getHours() {
+
+		return hourstodo;
+
+	}
+
+	public void setComplete(boolean isComp) {
+
+		complete = isComp;
+
+	}
+
+	public boolean getComplete() {
+
+		return complete;
+
+	}
+
+	public void setType(String typeis) {
+
+		type = typeis;
+
+	}
+
+	public void setTypeHW(String typeis) {
+
+		type = "Homework";
+
+	}
+
+	public void setTypeTSK(String typeis) {
+
+		type = "Task";
+
+	}
+
+	public void setTypeMeeting(String typeis) {
+
+		type = "Meeting";
+
+	}
+
+	public String getType() {
+
+		return type;
+
+	}
+
+}
