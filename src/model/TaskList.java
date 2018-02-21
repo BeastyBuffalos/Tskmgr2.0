4002d55d5b070d43402c622af19928c7d6730e76
Evan Overweg <ejoverwe@mtu.edu>
2018-02-21 13:50:52 -0500

    Creating TaskList code

--- src/model/TaskList.java
+++ src/model/TaskList.java
@@ -2,8 +2,24 @@ package model;
 
 public class TaskList {
 
+	
 	public TaskList() {
-		// TODO Auto-generated constructor stub
+		//TODO
 	}
+	
+	
 
+	private class TaskWrapper
+	{
+		private Task t;
+		private boolean moved; 
+		public TaskWrapper(Task t)
+		{
+			this.t = t;
+		}
+		
+		public Task get() { return t; }
+		
+		public void setMoved(boolean moved) { this.moved = moved; }
+	}
 }
