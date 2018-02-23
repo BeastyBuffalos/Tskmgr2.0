c44de51670e7d123aa4c626951cc89038e7fe86f
Evan Overweg <ejoverwe@mtu.edu>
2018-02-23 13:25:59 -0500

    Migrating sorting methods from TaskSort into TaskList

--- src/model/TaskList.java
+++ src/model/TaskList.java
@@ -1,6 +1,8 @@
 package model;
 
 import java.util.*;
+import Structures.OrderedPQ;
+import Structures.PQEntry;
 
 /**
  * 
@@ -8,34 +10,49 @@ import java.util.*;
  *
  */
 public class TaskList {
-	List<TaskWrapper> tasks = new LinkedList<TaskWrapper>(); //serializable?
-	HashMap<Task, TaskWrapper> taskMap = new HashMap<Task, TaskWrapper>(); //find different structures to use
+	OrderedPQ tasks = new OrderedPQ();
 	
 	public TaskList() {
 		//TODO
 	}
 	
-	public void addTask(Task t)
-	{
-		TaskWrapper wrap = new TaskWrapper(t);
-		boolean success = (taskMap.put(t, wrap) == null);
-		sort(wrap);
+	public void insertTask(Task task){
+		OrderedPQ pq = Singleton.INSTANCE.getPQ();
+		tasks.insert(Weighting.alg(task.getDifficulty(), task.getDue(), task.getHours(), task.getType(), task.getComplete()),task);
+		Singleton.INSTANCE.setPQ(pq);
+		return pq;
 	}
 	
-	public void moveTask(Task t, Task after)
-	{
-		TaskWrapper wrapT = taskMap.get(t), wrapAfter = taskMap.get(after);
-		
+	public OrderedPQ<K, V> removeTask(Task task){
+		OrderedPQ pq = Singleton.INSTANCE.getPQ();
+		int size1 = pq.size();
+		OrderedPQ<K,V> pq2 = new OrderedPQ<K,V>();
+		while(!(pq.isEmpty())){
+			PQEntry<K,V> removed = pq.removeMin();
+			if (removed.getValue() != task)
+				pq2.insert(removed.getKey(), removed.getValue());
+		}
+		if (pq.size() == size1)
+			System.out.println("Invalid task to remove.");
+		Singleton.INSTANCE.setPQ(pq2);
+		return pq2;
 	}
 	
-	/**
-	 * Helper method to place a task in the proper position.
-	 * @param t - the task to place
-	 */
-	private void sort(TaskWrapper t)
-	{
-		//TODO
+	public OrderedPQ<K, V> editTask(Task task, String name1, String type1, int due, int hours, boolean comp, int diff){
+		OrderedPQ pq = Singleton.INSTANCE.getPQ();
+		OrderedPQ<K,V> pq2 = new OrderedPQ<K,V>();
+		while(!(pq.isEmpty())){
+			PQEntry<K,V> removed = pq.removeMin();
+			if (removed.getValue() != task)
+				pq2.insert(removed.getKey(), removed.getValue());
+			else{
+				pq.insert(Weighting.alg(task.getDifficulty(), task.getDue(), task.getHours(), task.getType(), task.getComplete()),task);
+			}
+		}
+		Singleton.INSTANCE.setPQ(pq2);
+		return pq2;
 	}
+
 
 	/**
 	 * 
