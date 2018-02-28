2d80d701b001f5e20ae2d40fd2e2a5432f56280b
Evan Overweg <ejoverwe@mtu.edu>
2018-02-28 13:16:12 -0500

    Creating controller

---
+++ src/controller/TaskMgrDriver.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package controller;
+
+/**
+ * @author ejoverwe
+ *
+ */
+public class TaskMgrDriver {
+	
+	
+	/**
+	 * 
+	 */
+	private TaskMgrDriver() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+}
