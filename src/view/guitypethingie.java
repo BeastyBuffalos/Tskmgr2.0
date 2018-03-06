d9d3a01e84315b73e50c25a8b7ce6bc5bc3e57a2
jhschult <jhschult@mtu.edu>
2018-03-05 23:09:46 -0500

    attempted to add new task screen, kind of added exisiting tasks screen

--- src/view/guitypethingie.java
+++ src/view/guitypethingie.java
@@ -7,16 +7,20 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -27,7 +31,9 @@ public class guitypethingie {
 
 	public static void main(String[] args) {
 	
-		JFrame window = new JFrame();
+		JFrame window = new JFrame("Task Manager 2.0");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		JPanel windowpanel = new JPanel();
 		windowpanel.setLayout(new BorderLayout());
 		
@@ -58,9 +64,22 @@ public class guitypethingie {
 		
 		window.add(windowpanel);
 		
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
 		window.setVisible(true);
+		
+		
 		newtask.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent arg0) {
+				
+				makingtask(window);
+			
+			}
+		
+		
+		});
+		
+		existing.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent arg0) {
 				
@@ -72,43 +91,87 @@ public class guitypethingie {
 		});
 		
 		
-		
 	}
 	
-	public static void exist( JFrame window) {
+	public static void exist( JFrame window ) {
 		
-		//JFrame window1 = new JFrame();
+		JFrame window2 = window;
 		JPanel windowpanel1 = new JPanel();
 		windowpanel1.setLayout(new BorderLayout());
 		
 		
-		window.setSize(900, 400);
+		window2.setSize(900, 400);
 		
 		JPanel weltxt1 = new JPanel();
 		
-		JLabel welcome1 = new JLabel("its changed");
+		JLabel welcome1 = new JLabel("Here are the Existing Tasks. Please choose which one you wish to view.");
 		
 		JPanel buttons1 = new JPanel();
 		
-		JButton newtask1 = new JButton("New Task");
-		JButton existing1 = new JButton("Existing Tasks");
-		
 		window.setLayout(new CardLayout());
 		
-		buttons1.setLayout(new FlowLayout());
+		buttons1.setLayout(new BoxLayout(buttons1, BoxLayout.Y_AXIS));
 		
-		buttons1.add(newtask1);
-		buttons1.add(existing1);
-		
+		addAButton("Task 1", buttons1);
+		addAButton("Task 2", buttons1);
+		addAButton("Task 3", buttons1);
+		addAButton("Task 4", buttons1);
+		addAButton("Task 5", buttons1);
+		addAButton("Task 6", buttons1);
+		addAButton("Task 7", buttons1);
+		addAButton("Task 8", buttons1);
 		weltxt1.add(welcome1);
 		
-		windowpanel1.add(buttons1, BorderLayout.SOUTH);
-		windowpanel1.add(weltxt1, BorderLayout.CENTER);
+		windowpanel1.add(buttons1, BorderLayout.CENTER);
+		windowpanel1.add(weltxt1, BorderLayout.NORTH);
 		
-		window.add(windowpanel1);
+		window2.add(windowpanel1);
 		
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		window.setVisible(true);
+		window2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window2.setVisible(true);
+		
+	}
+	
+	private static void addAButton(String text, Container container) {
+        JButton button = new JButton(text);
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        container.add(button);
+    }
+
+	private static void addATextField(String text, Container container) {
+        JTextField field = new JTextField(text, 60);
+        field.setAlignmentX(Component.CENTER_ALIGNMENT);
+        container.add(field);
+    }
+	
+	public static void makingtask(JFrame window) {
+		
+		JFrame window2 = window;
+		
+		JPanel windowpanel1 = new JPanel();
+		windowpanel1.setLayout(new BorderLayout());
+		
+		window2.setSize(900, 400);
+		
+		JPanel tasktypes = new JPanel();
+		
+		JLabel newtask = new JLabel("Please input the specified information into the designated boxes.");
+		
+		addATextField("The Date Due:", tasktypes);
+		addATextField("How You Would It's Difficulty:", tasktypes);
+		addATextField("How Many Hours You Suspect It Will Take:", tasktypes);
+		addATextField("The Type of Task:", tasktypes);
+		addATextField("The Name of the Task:", tasktypes);
+	
+		windowpanel1.add(tasktypes, BorderLayout.CENTER);
+		windowpanel1.add(newtask, BorderLayout.NORTH);
+		
+		
+		window2.add(windowpanel1);
+		
+		window2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window2.setVisible(true);
+		
 		
 	}
 	
