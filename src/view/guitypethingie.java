d340f25baba7f9f671e2a7a38724f0e8f01f4c79
jhschult <jhschult@mtu.edu>
2018-03-07 13:58:15 -0500

    worked on new task page, attempted to add a back button to existing page, but with minor to no success

--- src/view/guitypethingie.java
+++ src/view/guitypethingie.java
@@ -21,6 +21,7 @@ import java.awt.CardLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -29,21 +30,19 @@ import java.awt.event.ActionListener;
 
 public class guitypethingie {
 
-	public static void main(String[] args) {
-	
-		JFrame window = new JFrame("Task Manager 2.0");
-		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	public static void starterup(JFrame window) {
 		
 		JPanel windowpanel = new JPanel();
 		windowpanel.setLayout(new BorderLayout());
 		
 		
-		window.setSize(900, 400);
+		window.setSize(1200, 800);
 		
 		JPanel weltxt = new JPanel();
 		
 		JLabel welcome = new JLabel("Welcome to TaskManager 2.0!"
-				+ " What would you like to do today?");
+				+ "    What would you like to do today?");
+		JLabel welcome2 = new JLabel("Create a new task, or view existing tasks?");
 		
 		JPanel buttons = new JPanel();
 		
@@ -58,6 +57,7 @@ public class guitypethingie {
 		buttons.add(existing);
 		
 		weltxt.add(welcome);
+		weltxt.add(welcome2);
 		
 		windowpanel.add(buttons, BorderLayout.SOUTH);
 		windowpanel.add(weltxt, BorderLayout.CENTER);
@@ -67,12 +67,12 @@ public class guitypethingie {
 		
 		window.setVisible(true);
 		
-		
 		newtask.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent arg0) {
 				
-				makingtask(window);
+				window.setVisible(false);
+				makingtask();
 			
 			}
 		
@@ -83,7 +83,8 @@ public class guitypethingie {
 
 			public void actionPerformed(ActionEvent arg0) {
 				
-				exist(window);
+				window.setVisible(false);
+				exist();
 			
 			}
 		
@@ -93,14 +94,21 @@ public class guitypethingie {
 		
 	}
 	
-	public static void exist( JFrame window ) {
+	public static void main(String[] args) {
+	
+		JFrame window = new JFrame("Task Manager 2.0");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		starterup(window);
+	}
+	
+	public static void exist( ) {
 		
-		JFrame window2 = window;
+		JFrame window2 = new JFrame();
 		JPanel windowpanel1 = new JPanel();
-		windowpanel1.setLayout(new BorderLayout());
 		
+		windowpanel1.setLayout(new BoxLayout(windowpanel1, BoxLayout.Y_AXIS));
 		
-		window2.setSize(900, 400);
+		window2.setSize(1200, 800);
 		
 		JPanel weltxt1 = new JPanel();
 		
@@ -108,25 +116,52 @@ public class guitypethingie {
 		
 		JPanel buttons1 = new JPanel();
 		
-		window.setLayout(new CardLayout());
+		window2.setLayout(new BorderLayout());
 		
-		buttons1.setLayout(new BoxLayout(buttons1, BoxLayout.Y_AXIS));
+		buttons1.setLayout(new GridLayout(16, 1));
+		buttons1.setPreferredSize(new Dimension((int)10,10));
 		
 		addAButton("Task 1", buttons1);
+		addLabel("", buttons1);
 		addAButton("Task 2", buttons1);
+		addLabel("", buttons1);
 		addAButton("Task 3", buttons1);
+		addLabel("", buttons1);
 		addAButton("Task 4", buttons1);
+		addLabel("", buttons1);
 		addAButton("Task 5", buttons1);
+		addLabel("", buttons1);
 		addAButton("Task 6", buttons1);
+		addLabel("", buttons1);
 		addAButton("Task 7", buttons1);
+		addLabel("", buttons1);
 		addAButton("Task 8", buttons1);
 		weltxt1.add(welcome1);
 		
-		windowpanel1.add(buttons1, BorderLayout.CENTER);
-		windowpanel1.add(weltxt1, BorderLayout.NORTH);
+		windowpanel1.add(weltxt1);
+		windowpanel1.add(buttons1);
 		
-		window2.add(windowpanel1);
+		JPanel back = new JPanel();
 		
+		JButton backtomenu = new JButton();
+		
+		back.add(backtomenu);
+		
+		window2.add(windowpanel1, BorderLayout.CENTER);
+		window2.add(back, BorderLayout.NORTH);
+		
+		backtomenu.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent arg0) {
+				
+				window2.setVisible(false);
+				
+				starterup(window2);
+			
+			}
+		
+		
+		});
 		window2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window2.setVisible(true);
 		
@@ -138,31 +173,43 @@ public class guitypethingie {
         container.add(button);
     }
 
-	private static void addATextField(String text, Container container) {
-        JTextField field = new JTextField(text, 60);
+	private static void addLabel(String text, Container container) {
+        JLabel field = new JLabel(text);
         field.setAlignmentX(Component.CENTER_ALIGNMENT);
         container.add(field);
     }
 	
-	public static void makingtask(JFrame window) {
+	private static void addATextField(String text, Container container) {
+        JTextField button = new JTextField(text, 60);
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        container.add(button);
+    }
+	
+	public static void makingtask() {
 		
-		JFrame window2 = window;
+		JFrame window2 = new JFrame();
+		window2.setSize(1200, 800);
 		
 		JPanel windowpanel1 = new JPanel();
 		windowpanel1.setLayout(new BorderLayout());
-		
-		window2.setSize(900, 400);
-		
+
 		JPanel tasktypes = new JPanel();
+		tasktypes.setLayout(new GridLayout(5,2));
+		tasktypes.setPreferredSize(new Dimension((int)10,10));
 		
 		JLabel newtask = new JLabel("Please input the specified information into the designated boxes.");
 		
-		addATextField("The Date Due:", tasktypes);
-		addATextField("How You Would It's Difficulty:", tasktypes);
-		addATextField("How Many Hours You Suspect It Will Take:", tasktypes);
-		addATextField("The Type of Task:", tasktypes);
-		addATextField("The Name of the Task:", tasktypes);
-	
+		addLabel("The Date Due: ", tasktypes);
+		addATextField("", tasktypes);
+		addLabel("How You Would Rate It's Difficulty: ", tasktypes);
+		addATextField("", tasktypes);
+		addLabel("How Many Hours You Suspect It Will Take: ", tasktypes);
+		addATextField("", tasktypes);
+		addLabel("The Type of Task: ", tasktypes);
+		addATextField("", tasktypes);
+		addLabel("The Name of the Task: ", tasktypes);
+		addATextField("", tasktypes);
+		
 		windowpanel1.add(tasktypes, BorderLayout.CENTER);
 		windowpanel1.add(newtask, BorderLayout.NORTH);
 		
