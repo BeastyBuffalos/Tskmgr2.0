5cfac8bee1325778290a403b32283493d6dfce64
jhschult <jhschult@mtu.edu>
2018-03-22 18:45:03 -0400

    added a back button, changed a bit of existing page, so instead of buttons, you type in the name of the task to get the info

--- src/view/guitypethingie.java
+++ src/view/guitypethingie.java
@@ -1,28 +1,16 @@
 package view;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.PrintWriter;
-import java.util.ArrayList;
-import java.util.Scanner;
 
 import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
 import javax.swing.JButton;
-import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
-import javax.swing.event.DocumentListener;
 import javax.swing.text.BadLocationException;
-import javax.swing.text.Document;
 import javax.swing.text.PlainDocument;
 
 import java.awt.BorderLayout;
 import java.awt.CardLayout;
-import java.awt.Color;
 import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
@@ -70,6 +58,7 @@ public class guitypethingie {
 		
 		
 		window.setVisible(true);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		newtask.addActionListener(new ActionListener() {
 
@@ -116,31 +105,32 @@ public class guitypethingie {
 		
 		JPanel weltxt1 = new JPanel();
 		
-		JLabel welcome1 = new JLabel("Here are the Existing Tasks. Please choose which one you wish to view.");
+		JLabel welcome1 = new JLabel("Here are the Existing Tasks. Please choose which one "
+				+ "you wish to view by typing in the task name in the given text box.");
 		
 		JPanel buttons1 = new JPanel();
 		
-		window2.setLayout(new BorderLayout());
+		window2.setLayout(new GridLayout(5,2));
 		
-		buttons1.setLayout(new GridLayout(16, 1));
-		buttons1.setPreferredSize(new Dimension((int)10,10));
+		buttons1.setLayout(new GridLayout(16, 2));
+		buttons1.setPreferredSize(new Dimension((int)1000000,1000000));
 		
 		//this is temporary
-		addAButton("Task 1", buttons1);
-		addLabel("", buttons1);
-		addAButton("Task 2", buttons1);
-		addLabel("", buttons1);
-		addAButton("Task 3", buttons1);
-		addLabel("", buttons1);
-		addAButton("Task 4", buttons1);
-		addLabel("", buttons1);
-		addAButton("Task 5", buttons1);
-		addLabel("", buttons1);
-		addAButton("Task 6", buttons1);
-		addLabel("", buttons1);
-		addAButton("Task 7", buttons1);
-		addLabel("", buttons1);
-		addAButton("Task 8", buttons1);
+		addLabel("Task 1", buttons1);
+		//addLabel("", buttons1);
+		addLabel("Task 2", buttons1);
+		//addLabel("", buttons1);
+		addLabel("Task 3", buttons1);
+		//addLabel("", buttons1);
+		addLabel("Task 4", buttons1);
+		//addLabel("", buttons1);
+		addLabel("Task 5", buttons1);
+		//addLabel("", buttons1);
+		addLabel("Task 6", buttons1);
+		//addLabel("", buttons1);
+		addLabel("Task 7", buttons1);
+		//addLabel("", buttons1);
+		addLabel("Task 8", buttons1);
 		weltxt1.add(welcome1);
 		
 		//try something like this
@@ -153,17 +143,31 @@ public class guitypethingie {
 		 * }
 		 */
 		
+		
+		
+		JPanel tasks = new JPanel();
+		
+		tasks.setLayout(new BoxLayout(tasks, BoxLayout.Y_AXIS));
+		
+		tasks.add(buttons1);
+		
 		windowpanel1.add(weltxt1);
-		windowpanel1.add(buttons1);
+		
+		JPanel textme = new JPanel();
+		
+		addATextField("", textme);
 		
 		JPanel back = new JPanel();
 		
-		JButton backtomenu = new JButton();
+		JButton backtomenu = new JButton("Back To Main Menu");
 		
 		back.add(backtomenu);
 		
-		window2.add(windowpanel1, BorderLayout.CENTER);
-		window2.add(back, BorderLayout.NORTH);
+		window2.add(back);
+		window2.add(windowpanel1);
+		
+		window2.add(tasks);
+		window2.add(textme);
 		
 		backtomenu.addActionListener(new ActionListener() {
 
@@ -171,12 +175,14 @@ public class guitypethingie {
 				
 				window2.setVisible(false);
 				
-				starterup(window2);
+				JFrame mainmenu = new JFrame();
+				starterup(mainmenu);
 			
 			}
 		
 		
 		});
+		
 		window2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		window2.setVisible(true);
 		
@@ -276,10 +282,17 @@ public class guitypethingie {
         enterbutton.setAlignmentX(Component.CENTER_ALIGNMENT);
         enter.add(enterbutton);
         
-        
+        JPanel back = new JPanel();
+		
+		JButton backtomenu = new JButton("Back To Main Menu");
+		
+		back.add(backtomenu);
+		
 		windowpanel1.add(tasktypes, BorderLayout.CENTER);
 		windowpanel1.add(newtask, BorderLayout.NORTH);
 		windowpanel1.add(enter, BorderLayout.SOUTH);
+		windowpanel1.add(back, BorderLayout.EAST);
+		
 		
 		window2.add(windowpanel1);
 		
@@ -291,8 +304,22 @@ public class guitypethingie {
 			public void actionPerformed(ActionEvent arg0) {
 				
 				//use the getText to get the text for the new task
-				System.out.println(listen1.getText());
+				//System.out.println(listen1.getText());
 			
+			}
+		
+		
+		});
+		
+		backtomenu.addActionListener(new ActionListener() {
+
+			public void actionPerformed(ActionEvent arg0) {
+				
+				window2.setVisible(false);
+				
+				JFrame mainmenu = new JFrame();
+				starterup(mainmenu);
+				
 			}
 		
 		
