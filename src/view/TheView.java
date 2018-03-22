7496fda664a8e0bd5d61127e1ec2e9158c930566
jhschult <jhschult@mtu.edu>
2018-03-22 19:05:21 -0400

    renaming guitypethingie to TheView

--- src/view/TheView.java
+++ src/view/TheView.java
@@ -20,7 +20,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
-public class guitypethingie {
+public class TheView {
 
 	public static void starterup(JFrame window) {
 		
