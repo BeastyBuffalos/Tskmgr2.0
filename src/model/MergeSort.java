fc7f0e33f73daf46174c66cb67ed146d15f7eb3b
jhschult <jhschult@mtu.edu>
2018-03-30 13:08:59 -0400

    Radix Sort Added and Merge Sort as well

---
+++ src/model/MergeSort.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
+public class MergeSort {
+
+	public static <E extends Comparable<E>> void sort(ArrayList<E> array) {
+
+		int n = array.size();
+		
+		if ( n < 2 ) {
+			return;
+		}
+		
+		int mid = n/2;
+		
+		ArrayList<E> s1 = new ArrayList<E>(array.subList(0, mid)); //new ArrayList<String>(al.subList(1, 4));
+		ArrayList<E> s2 = new ArrayList<E>(array.subList(mid, n));
+		
+		sort(s1);
+		sort(s2);
+		
+		merge(s1, s2, array);
+			}
+
+	public static <E extends Comparable<E>> void merge(ArrayList<E> s1, ArrayList<E> s2, ArrayList<E> array) {
+		
+		int i = 0; int j = 0;
+		
+		while( i + j < array.size()) {
+			if ( j == s2.size() || ( i < s1.size() && s1.get(i).compareTo(s2.get(j)) < 0))
+				array.add(i+j, s1.get(i++));
+			else
+				array.add(i+j, s2.get(j++));
+		}
+	}
+	
+}
