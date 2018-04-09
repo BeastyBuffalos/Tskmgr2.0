52ee166887ac649b9f503c420c0682978a3f15bd
jhschult <jhschult@mtu.edu>
2018-04-09 13:24:09 -0400

    just testing out how to fix the size of the drop down box I have just put in New Task panel

--- src/view/GraphicalView.java
+++ src/view/GraphicalView.java
@@ -343,17 +343,17 @@ public class GraphicalView {
 		enter.add(enterbutton);
 		enter.add(backtomenu);
 		
-//		
-//		String[] switchit = { "Main Menu", "New Task", "Edit Tasks"};
-//		JComboBox changescreen = new JComboBox(switchit);
-//		changescreen.setSelectedIndex(0);
-//		//changescreen.addActionListener(this);
-//		
-//		
-//		
-//		JPanel dropdown = new JPanel();
-//		dropdown.setLayout(new GridLayout(0,2));
-//		
+		
+		String[] switchit = { "Main Menu", "New Task", "Edit Tasks"};
+		JComboBox changescreen = new JComboBox(switchit);
+		changescreen.setSelectedIndex(0);
+		//changescreen.addActionListener(this);
+		
+		
+		
+		JPanel dropdown = new JPanel();
+		dropdown.setLayout(new GridLayout(0,2));
+		
 //		JLabel blank = new JLabel("");
 //		JLabel blank1 = new JLabel("");
 //		JLabel blank2 = new JLabel("");
@@ -363,26 +363,32 @@ public class GraphicalView {
 //		JLabel blank6 = new JLabel("");
 //		JLabel blank7 = new JLabel("");
 //		
-//		dropdown.add(changescreen);
-//		
-//		blank.setBorder(border);
+		dropdown.add(changescreen);
+		
+		//changescreen.setBounds(0, 0, 1, 1);
+		
+		Dimension preferredSize = new Dimension(1,1);
+//		changescreen.setPreferredSize(preferredSize);
+		
+		changescreen.setMaximumSize(preferredSize);
 //		blank.setFont(blank.getFont().deriveFont(Font.BOLD, 1));
 //		blank1.setFont(blank1.getFont().deriveFont(Font.BOLD, 1));
 //		blank2.setFont(blank2.getFont().deriveFont(Font.BOLD, 1));
 //		dropdown.add(blank);
 //		dropdown.add(blank1);
 //		dropdown.add(blank2);
-//		
-////		dropdown.add(blank4);
-////		dropdown.add(blank5);
-////		dropdown.add(blank6);
-////		dropdown.add(blank7);
-//		
-//		dropdown.add(newtask);
-//		dropdown.add(blank3);
-//		
+		
+//		dropdown.add(blank4);
+//		dropdown.add(blank5);
+//		dropdown.add(blank6);
+//		dropdown.add(blank7);
+		
+		dropdown.add(changescreen);
+		dropdown.add(newtask);
+		
+		
 		windowpanel1.add(tasktypes, BorderLayout.CENTER);
-		windowpanel1.add(newtask, BorderLayout.NORTH);
+		windowpanel1.add(dropdown, BorderLayout.NORTH);
 		windowpanel1.add(enter, BorderLayout.SOUTH);
 		windowpanel1.add(space, BorderLayout.EAST);
 		
